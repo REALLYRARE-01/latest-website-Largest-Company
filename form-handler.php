84dc59bad37fba17da0f29c9ae2fe873b341824e
REALLYRARE-01 <111707496+REALLYRARE-01@users.noreply.github.com>
2022-10-02 16:12:48 +0200

    first commit

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'BigProgram@gmail.com';
+$email_from = 'universalsoul623@gmail.com';
 
 $email_subject = 'New Form Submission';
 
